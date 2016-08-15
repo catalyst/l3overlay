83016a3cd9a27e1032e997ba1f796439b25293a6
Callum Dickinson <callum.dickinson@catalyst.net.nz>
2016-08-15 18:32:44 +1200

    Add stub code for unit test

---
+++ src/l3overlay/tests/__init__.py
@@ -0,0 +1,56 @@
+#
+# IPsec overlay network manager (l3overlay)
+# l3overlay/tests/__init__.py - test base class and functions
+#
+# Copyright (c) 2016 Catalyst.net Ltd
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+import concurrencytest
+import unittest
+
+import mininet.net
+
+import mininet.node
+
+import mininet.topo
+
+
+class L3overlayTopo(mininet.topo.Topo):
+    '''
+    '''
+
+    def build(self):
+        '''
+        '''
+        pass
+
+
+class L3overlayTest(unittest.TestCase):
+    '''
+    '''
+
+    def setUp(self):
+        '''
+        '''
+
+        pass
+
+
+    def tearDown(self):
+        '''
+        '''
+
+        pass
